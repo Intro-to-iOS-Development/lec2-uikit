91aeea6ff0a8c0748a14be40aa82bf6de1e8a8b8
Vin Bui <vdb23@cornell.edu>
2024-03-18 22:44:37 -0400

    Finish UILabel

--- README.md
+++ README.md
@@ -1,6 +1,2 @@
 # lec2-uikit
 Demo code for Lecture 2: UIKit + AutoLayout
-
-There are 2 branches:
-- `1-uilabel`: implement `UILabel`
-- `2-uiimageview`: implement `UIImageView`
--- lec2/ViewController.swift
+++ lec2/ViewController.swift
@@ -8,12 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: - Properties (view)
+    
+    // 1. Create the view property
+    private let nameLabel = UILabel()
+    
+    // MARK: - viewDidLoad
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = UIColor.white
+        
+        // 6. Call setup function
+        setupNameLabel()
+    }
+    
+    // MARK: - Set Up Views
+    
+    // 2. Setup the view
+    private func setupNameLabel() {
+        // 3. Configure the view's properties
+        nameLabel.text = "Vin"
+        nameLabel.font = .systemFont(ofSize: 24)
+        nameLabel.textColor = UIColor.black
+        
+        // 4. Add view as a subview and enable auto-layout
+        view.addSubview(nameLabel)
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        // 5. Set Up Constraints
+        NSLayoutConstraint.activate([
+            nameLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            nameLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
     }
 
-
 }
-
