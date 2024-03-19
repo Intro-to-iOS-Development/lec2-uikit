741cf0c7bac089ee28df249f8df9378fe5662823
Vin Bui <vdb23@cornell.edu>
2024-03-18 22:43:44 -0400

    Starter code

---
+++ lec2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lec2
+//
+//  Created by Vin Bui on 9/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
